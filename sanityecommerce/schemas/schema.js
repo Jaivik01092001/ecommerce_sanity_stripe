9bd66c093a2a4800d56fd2056ef9995581d6e6da
Patel Jaivik <19bece54024@vsitr.ac.in>
2023-03-04 10:49:12 +0530

    first commit

---
+++ sanityecommerce/schemas/schema.js
@@ -0,0 +1,11 @@
+// import createSchema from 'part:@sanity/base/schema-creator';
+// import {schemaTypes} from './schemas'
+
+// import product from './product'
+// import banner from './banner'
+
+// export default createSchema({
+//     name : 'default',
+//     types : schemaTypes.concat([product,banner])
+// })
+
